e91d5b72ff7754db6b374eb22418cc256b1060e1
Derrak Richard <derrak@gmail.com>
2022-06-03 11:58:14 -0700

    add value logic to radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
   $("#formOne").submit(function() {
-    $(".person1").append("blah blah");
-    $(".person2").append("blah blah");
-    $(".animal").append("blah blah");
-    $(".exclamation").append("blah blah");
-    $(".verb").append("blah blah");
-    $(".noun").append("blah blah");
+    event.preventDefault();
+    const image1Val = parseInt($("input[name='image1']:checked").val());
+    console.log("image1Val = " + image1Val);
 
-    $("#story").show();
+    
+    const score = image1Val;
 
+  // $("#story").show();
 
-  const image1Val = $("input:radio[name=image1]:checked").val().parseInt();
-  console.log(image1Val);
-  
+    if (score >= 14) {
+      $('#HighScore').show();
+    }
+    else if (score >= 7) {
+      $('#MediumScore').show();
+    }
+    else {
+      $('#LowScore').show();
+    }
+  });
 });
+
+
+
