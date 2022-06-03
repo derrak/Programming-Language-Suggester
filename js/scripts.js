b71f2675e2d081c71224ea4714f9190034f90107
Derrak Richard <derrak@gmail.com>
2022-06-03 14:43:44 -0700

    add 5 questons; update logic to reflect scores from all questions

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,28 @@ $(document).ready(function() {
     event.preventDefault();
     const image1Val = parseInt($("input[name='image1']:checked").val());
     const image2Val = parseInt($("input[name='image2']:checked").val());
+    const image3Val = parseInt($("input[name='image3']:checked").val());
+    const image4Val = parseInt($("input[name='image4']:checked").val());
+    const image5Val = parseInt($("input[name='image5']:checked").val());
+    const totalScore = image1Val+image2Val+image3Val+image4Val+image5Val;
 
-    const totalScore = image1Val+image2Val;
-
-      
-    console.log("image1Val = " + image1Val);
-    console.log("image2Val = " + image2Val);
-    console.log("totalScore = " + totalScore);
-
-  // $("#story").show();
-
-    if (totalScore >= 14) {
+    if (totalScore >= 7) { //Ruby
+      $('#MediumScorem').hide();
+      $('#LowScore').hide();
       $('#HighScore').show();
+
     }
-    else if (totalScore >= 7) {
+    else if (totalScore >= 5) { //Python
+      $('#HighScore').hide();
+      $('#LowScore').hide();
       $('#MediumScore').show();
     }
-    else {
+    else { //Swift
+      $('#HighScore').hide();
+      $('#MediumScore').hide();
       $('#LowScore').show();
     }
+
   });
 });
 
